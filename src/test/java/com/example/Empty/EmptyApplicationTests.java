6f6e210118aefaca711b559a4f2315b9931eb56b
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-21 19:55:32 +0600

    Initial commit

---
+++ src/test/java/com/example/Empty/EmptyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Empty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
