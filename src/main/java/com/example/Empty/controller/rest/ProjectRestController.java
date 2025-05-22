d9817c37d97d0da122bef186e1d7316902d18145
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-23 00:00:34 +0600

    move to rest sub-package and introduce @Autowired for inject dependency.

--- src/main/java/com/example/Empty/controller/rest/ProjectRestController.java
+++ src/main/java/com/example/Empty/controller/rest/ProjectRestController.java
@@ -1,8 +1,10 @@
-package com.example.Empty.controller;
+package com.example.Empty.controller.rest;
 
 import com.example.Empty.model.domain.Person;
 import com.example.Empty.model.dto.CreatePersonRequest;
+import com.example.Empty.service.PersonService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,21 +16,19 @@ import java.util.List;
 @RestController
 public class ProjectRestController {
 
-    public List<Person> personList=new ArrayList<>();
-
+    @Autowired
+    PersonService personService;
 
     @Tag(name = "Get Person List", description = "Get all the person list at a time")
     @GetMapping("api/persons")
     public List<Person> getAllPerson() {
-        return personList;
+        return personService.getAllPerson();
     }
 
     @Tag(name = "Create a project", description = "Create a new Project")
     @PostMapping("api/persons")
     public Person createPerson(@RequestBody CreatePersonRequest personDTO) {
-        Person person=new Person(personDTO.getfName(), personDTO.getlName());
-        personList.add(person);
-        return person;
+        return  personService.createPerson(personDTO);
     }
 
 }
