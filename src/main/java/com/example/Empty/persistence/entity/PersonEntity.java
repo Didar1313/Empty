4211dc6d840d5eff87d8219edc1d53826fe7eb94
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-22 20:36:52 +0600

    implement PersonEntity in persistence package to handle data

---
+++ src/main/java/com/example/Empty/persistence/entity/PersonEntity.java
@@ -0,0 +1,37 @@
+package com.example.Empty.persistence.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class PersonEntity {
+    @Id
+    private Long id;
+    private String fName;
+    private String lName;
+
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
