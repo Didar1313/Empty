9ef51a903bbd78cee2a1e2f8a54154621c982391
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-23 01:37:34 +0600

    update createPerson and getAllPerson method

--- src/main/java/com/example/Empty/service/PersonService.java
+++ src/main/java/com/example/Empty/service/PersonService.java
@@ -2,21 +2,41 @@ package com.example.Empty.service;
 
 import com.example.Empty.model.domain.Person;
 import com.example.Empty.model.dto.CreatePersonRequest;
+import com.example.Empty.persistence.entity.PersonEntity;
+import com.example.Empty.persistence.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Service
 public class PersonService {
-    public List<Person> personList=new ArrayList<>();
+
+    @Autowired
+     PersonRepository personRepository;
+
 
     public List<Person>getAllPerson(){
-        return personList;
+        List<PersonEntity> personEntities= personRepository.findAll();
+        return personEntities.stream().map(personEntity -> {
+            Person person = new Person(personEntity.getId(),personEntity.getfName(),personEntity.getlName());
+            return person;
+        }).toList();
     }
+
     public Person createPerson(CreatePersonRequest createPersonRequest){
-        Person person=new Person(createPersonRequest.getfName(),createPersonRequest.getlName());
-        personList.add(person);
-        return person;
+
+
+        PersonEntity entity = new PersonEntity();
+        entity.setfName(createPersonRequest.getfName());
+        entity.setlName(createPersonRequest.getlName());
+
+        PersonEntity savedEntity = personRepository.save(entity);
+
+        Long id = savedEntity.getId();
+        String fName = savedEntity.getfName();
+        String lName = savedEntity.getlName();
+        return new Person(id, fName, lName);
     }
 }
