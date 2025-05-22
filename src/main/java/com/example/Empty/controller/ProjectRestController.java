eaef56b8bdd88e338ea2d9c407a9f55da4cca395
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-22 20:40:38 +0600

    add @Tag for easily understand post,create,put,delete etc

--- src/main/java/com/example/Empty/controller/ProjectRestController.java
+++ src/main/java/com/example/Empty/controller/ProjectRestController.java
@@ -2,6 +2,7 @@ package com.example.Empty.controller;
 
 import com.example.Empty.model.domain.Person;
 import com.example.Empty.model.dto.CreatePersonRequest;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +17,13 @@ public class ProjectRestController {
     public List<Person> personList=new ArrayList<>();
 
 
-
+    @Tag(name = "Get Person List", description = "Get all the person list at a time")
     @GetMapping("api/persons")
     public List<Person> getAllPerson() {
         return personList;
     }
 
+    @Tag(name = "Create a project", description = "Create a new Project")
     @PostMapping("api/persons")
     public Person createPerson(@RequestBody CreatePersonRequest personDTO) {
         Person person=new Person(personDTO.getfName(), personDTO.getlName());
