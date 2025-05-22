3f6e7a0b54f53ffeebec5d8bb31086f9fffe0896
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-23 01:33:59 +0600

    Add @id,@GeneratedValue annotation to make it unique

--- src/main/java/com/example/Empty/persistence/entity/PersonEntity.java
+++ src/main/java/com/example/Empty/persistence/entity/PersonEntity.java
@@ -1,11 +1,13 @@
 package com.example.Empty.persistence.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
 public class PersonEntity {
     @Id
+    @GeneratedValue
     private Long id;
     private String fName;
     private String lName;
