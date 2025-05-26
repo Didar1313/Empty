96bc7081ec990fafb135b31b3c27592e897775f9
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-26 17:51:15 +0600

    Add handling for invalid method arguments in exceptions for MethodArgumentNotValidException and MethodArgumentTypeMismatchException in the GlobalExceptionHandler.

--- src/main/java/com/example/Empty/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Empty/exception/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package com.example.Empty.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,5 +13,11 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleNotFoundException(NotFoundException e) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_GATEWAY, e.getMessage());
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ProblemDetail handleMethodArgumentNotValidException(MethodArgumentTypeMismatchException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_GATEWAY, "Not Valid Input");
+    }
+
 
 }
