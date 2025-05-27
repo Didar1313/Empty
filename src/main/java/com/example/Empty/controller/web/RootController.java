13ca66edc5df69a80b777c0fcabc68f70d3ab8c7
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-27 23:24:15 +0600

    get single properties using @Value

--- src/main/java/com/example/Empty/controller/web/RootController.java
+++ src/main/java/com/example/Empty/controller/web/RootController.java
@@ -1,16 +1,11 @@
 package com.example.Empty.controller.web;
-
 import com.example.Empty.config.ResumeConfig;
-import com.example.Empty.personalInfoData.Education;
-import com.example.Empty.personalInfoData.Experience;
-import com.example.Empty.personalInfoData.PersonalInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class RootController {
@@ -18,40 +13,16 @@ public class RootController {
     @Autowired
     ResumeConfig resumeConfig;
 
+    @Value("${site.title}")
+    String siteTitle;
+
+    @Value("${site.description}")
+    String siteDescription;
+
     @GetMapping("/")
     public String index(Model model) {
-
-//        //Personal Info
-//        PersonalInfo personalInfo=new PersonalInfo(
-//                "Didar Bhuiyan", "didarbhuiyan1313@gmail.com",
-//                "https://www.linkedin.com/in/md-didar-bhuiyan/",
-//                "https://github.com/Didar1313");
-//        model.addAttribute("personalInfo", personalInfo);
-//
-//        //Education
-//        List<Education> education= Arrays.asList(
-//                new Education(
-//                        "BSc in CSE", "Green University of Bangladesh",
-//                        "2021 - 2025", "Working as a Backend Developer"));
-//        model.addAttribute("education", education);
-//
-//        //Experience
-//        List<Experience> experience= Arrays.asList(
-//                new Experience(
-//                        "Software Engineer", "Empty",
-//                        "2025 - Present",
-//                        "Working as a Backend Developer"
-//                ),
-//                new Experience(
-//                        "Junior Developer", "Wizard Tech Banglaesh Ltd",
-//                        "2024", "Working as a ..."
-//                )
-//        );
-//        model.addAttribute("experience", experience);
-//
-//        // Skills
-//        List<String> skills = Arrays.asList("Java", "Spring Boot", "Thymeleaf");
-//        model.addAttribute("skills", skills);
+        model.addAttribute("siteTitle", siteTitle);
+        model.addAttribute("siteDescription", siteDescription);
 
         model.addAttribute("personalInfo", resumeConfig.getPersonalInfo());
         model.addAttribute("education", resumeConfig.getEducation());
