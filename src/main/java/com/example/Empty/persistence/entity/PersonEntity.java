ff0e23e04ff4e9a58e3d3451645c40d81e53f388
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-28 21:56:41 +0600

    Add Lombok annotations to simplify PersonEntity class

--- src/main/java/com/example/Empty/persistence/entity/PersonEntity.java
+++ src/main/java/com/example/Empty/persistence/entity/PersonEntity.java
@@ -3,37 +3,18 @@ package com.example.Empty.persistence.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
+import lombok.Getter;
+import lombok.Setter;
+@Getter
 @Entity
 public class PersonEntity {
     @Id
     @GeneratedValue
     private Long id;
+    @Setter
     private String fName;
+    @Setter
     private String lName;
 
 
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
