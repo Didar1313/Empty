41df49a7d035f3e79586fddcad760a6d8ab229f0
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-28 21:47:00 +0600

    Replace boilerplate getter, setter, and constructor code with Lombok annotations (@Getter, @AllArgsConstructor, @NoArgsConstructor) for improved readability and maintainability. This eliminates manual management of these methods.

--- src/main/java/com/example/Empty/model/domain/Person.java
+++ src/main/java/com/example/Empty/model/domain/Person.java
@@ -1,6 +1,12 @@
 package com.example.Empty.model.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
 
     private Long id;
@@ -8,37 +14,5 @@ public class Person {
     private String lName;
 
 
-    public Person(Long id, String fName, String lName) {
-        this.id = id;
-        this.fName = fName;
-        this.lName = lName;
-    }
 
-    public Person() {
-
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
