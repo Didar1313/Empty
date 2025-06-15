115eef93c4df52672cd706343dc7b279f71756a5
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-06-16 01:08:58 +0600

    Add dynamic person data and projects section to index
    Integrated PersonService into RootController to fetch and display dynamic person data in the index view. Updated the HTML template to include a new "Projects" section, iterating through person data to display relevant information.

--- src/main/java/com/example/Empty/controller/web/RootController.java
+++ src/main/java/com/example/Empty/controller/web/RootController.java
@@ -1,5 +1,8 @@
 package com.example.Empty.controller.web;
 import com.example.Empty.config.ResumeConfig;
+import com.example.Empty.controller.rest.ProjectRestController;
+import com.example.Empty.model.domain.Person;
+import com.example.Empty.service.PersonService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +11,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Controller
 public class RootController {
 
     @Autowired
     ResumeConfig resumeConfig;
+
+    @Autowired
+    PersonService personService;
+
 
     @Value("${site.title}")
     String siteTitle;
@@ -25,14 +35,17 @@ public class RootController {
 
     @GetMapping("/")
     public String index(Model model) {
+
         logger.info("Index page is called");
+
         model.addAttribute("siteTitle", siteTitle);
         model.addAttribute("siteDescription", siteDescription);
-
         model.addAttribute("personalInfo", resumeConfig.getPersonalInfo());
         model.addAttribute("education", resumeConfig.getEducation());
         model.addAttribute("experience", resumeConfig.getExperience());
         model.addAttribute("skills", resumeConfig.getSkills());
+        model.addAttribute("persons" , personService.getAllPerson());
+
         logger.info("Index page is rendered");
 
         return "index";
