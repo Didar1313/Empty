1519d0218fb204a9f7aba71be625733260e10a59
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-06-25 23:20:04 +0600

    Execute query by data and schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS post_en (
+                                       id BIGINT PRIMARY KEY,
+                                       title VARCHAR(255),
+                                       content CLOB,
+                                       author VARCHAR(255),
+                                       created_at TIMESTAMP
+);
