8f698537be205c45bcac9eb030ed2d066b1f3c2b
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-07-09 02:48:27 +0600

    remove manual query.

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS post_en (
-                                       id BIGINT PRIMARY KEY,
-                                       title VARCHAR(255),
-                                       content CLOB,
-                                       author VARCHAR(255),
-                                       created_at TIMESTAMP
-);
