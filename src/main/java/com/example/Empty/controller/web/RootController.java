300f820f2cf0c83cfa8d66c5195b9b4c921559fd
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-07-08 21:21:36 +0600

    ref: make home page

--- src/main/java/com/example/Empty/controller/web/RootController.java
+++ src/main/java/com/example/Empty/controller/web/RootController.java
@@ -1,30 +1,31 @@
 package com.example.Empty.controller.web;
 
-import com.example.Empty.persistence.blogEntity.PostEn;
-import com.example.Empty.persistence.repository.blogRepo.PostEnRepository;
-import org.springframework.data.domain.Page;
+import com.example.Empty.model.domain.Post;
+import com.example.Empty.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+@RequestMapping({"/"})
 @Controller
 public class RootController {
 
-    private final PostEnRepository postRepository;
+    @Autowired
+    PostService postService;
 
-    public RootController(PostEnRepository postRepository) {
-        this.postRepository = postRepository;
-    }
-
-    @GetMapping("/")
+    @GetMapping
     public String home(Model model) {
-        Pageable topTwo = PageRequest.of(0, 2, Sort.by(Sort.Direction.DESC, "createdAt"));
-        Page<PostEn> recentPosts = postRepository.findAll(topTwo);
-
-        model.addAttribute("posts", recentPosts.getContent());
+        Pageable topTwo = PageRequest.of(0, 2, Sort.by(Sort.Direction.DESC, "publishedAt"));
+        List<Post> posts = postService.getAllPost(topTwo);
+        model.addAttribute("posts", posts);
         return "blog/index"; // This points to templates/index.html
     }
+
+
 }
