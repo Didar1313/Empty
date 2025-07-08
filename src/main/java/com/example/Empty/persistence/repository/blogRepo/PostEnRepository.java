ee56118f15feafcc4f9389c9c719c830dcb98db6
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-07-08 21:24:03 +0600

    remove PostEn.java, PostEnRepository.java

--- src/main/java/com/example/Empty/persistence/repository/blogRepo/PostEnRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Empty.persistence.repository.blogRepo;
-
-import com.example.Empty.persistence.blogEntity.PostEn;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostEnRepository extends JpaRepository<PostEn, Long> {
-}
